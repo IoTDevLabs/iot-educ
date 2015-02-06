67aeb9b8f089c5ee1711441d5368d896eeea7ab0
Greg Toth <gtoth@iotdc.org>
2015-02-05 21:38:04 -0500

    initial commit

---
+++ rpi/grove-buzzer.py
@@ -0,0 +1,33 @@
+############################################################
+# Simple Python program that repeatedly beeps a
+# Grove Buzzer attached to port D2.
+#
+# To run this program from the bash command prompt,
+# type this and hit Enter:
+#
+#    python grove_buzzer.py
+#
+############################################################
+
+import time
+import grovepi
+
+buzzer=2                               # Buzzer connected to D2 port
+
+while True:
+  
+  try:
+    grovepi.digitalWrite(buzzer, 1)    # buzzer on
+    print "buzzer on"
+    time.sleep(0.5)
+
+    grovepi.digitalWrite(buzzer, 0)    # buzzer off
+    print "buzzer off"
+    time.sleep(0.5)
+
+  except KeyboardInterrupt:
+    grovepi.digitalWrite(buzzer, 0)
+    break
+  except IOError:
+    print "IOError"
+
