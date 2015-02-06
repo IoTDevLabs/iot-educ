7f10ce972d17e5824a8fc4fa26bddecda24fd69d
Greg Toth <gtoth@iotdc.org>
2015-02-05 21:38:11 -0500

    initial commit

---
+++ rpi/grove-ultrasonic-piland.py
@@ -0,0 +1,53 @@
+############################################################
+# Simple Python program that repeatedly reads the Grove
+# Ultrasonic Ranger connected to GrovePi port D4 and
+# writes the current value to Pi Land. 
+#
+# To run this program from the bash command prompt,
+# type this and hit Enter:
+#
+#    python grove-ultrasonic-piland.py
+#
+############################################################
+
+import time
+import requests
+import grovepi
+
+# Write the value to a specific data slot in a Pi Land room
+
+# NOTE:  Change the room, dataslot, and devicename below to something
+#        different for your own use so that everyone isn't using
+#        the same data slot and overwriting each other's data.
+
+room = 404                             # Room number to use (1 through 999)
+dataslot = 21                          # Data slot number to use (1 through 30)
+devicename = "Distance"                # Descriptive name for your device, put '+' for space char
+
+baseurl = "http://piland.socialdevices.io"
+baseurl = baseurl + "/" + str(room) + "/write/" + str(dataslot) + "?name=" + devicename + "&value="
+
+ranger=4                               # Ultrasonic Ranger is connected to port D4
+
+while True:
+  
+  try:
+
+    # Read the ultrasonic ranger distance
+
+    distance_cm = grovepi.ultrasonicRead(ranger)
+
+    url = baseurl + "%d" % distance_cm + "+cm"
+
+    print url
+
+    requests.get(url)    # write data
+
+    time.sleep(2.0)      # 2 second delay
+
+  except KeyboardInterrupt:
+    print "Terminating"
+    break
+  except IOError:
+    print "IOError"
+
