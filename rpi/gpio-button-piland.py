1cf606c305fa5109a3e67bbd9dbc38409ef39b2b
Greg Toth <gtoth@>
2016-05-06 16:51:01 -0400

    move button processing into a class to allow multiple buttons to be created

--- rpi/gpio-button-piland.py
+++ rpi/gpio-button-piland.py
@@ -24,8 +24,8 @@
 #
 ############################################################
 
-# Button debouncing can be adjusted by changing the values
-# of button1_debounce_max and/or button1_debounce_delay.
+# Button debouncing can be adjusted by changing the value
+# of debounce_max and/or the time delay in the read loop.
 # Test your button to find the optimum settings for speed
 # versus accurate reading of press & release.
 
@@ -37,92 +37,97 @@ import time                    # Built-in package; no need to install
 import sys                     # Built-in package; no need to install
 import RPi.GPIO as GPIO        # Should already be installed on Pi
 
-# Sensor settings
-button1_gpio_number = 17       # Button 1 connected to GPIO 17 (pin 11) 
+# The Button class handles operations for one button.
 
-# Button debouncing
-button1_reading = -1           # Current reading from hardware pin
-button1_reading_previous = -1  # Previous reading from hardware pin
-button1_debounce_counter = 0   # Debounce counter
-button1_debounce_max = 2       # How many consecutive readings of same 0 or 1 state are needed
-button1_debounce_delay = 0.01  # Time (in seconds) to wait between button readings
-button1_stable_state = -1      # Debouced (stable) reading of the button
+class Button:
 
-# Pi Land room
-piland_room = 404              # Room number to use (1 through 999)
+  def __init__(self, name, gpio, room, onoff_slot, counter_slot, onoff_label, counter_label):
+    # Initialize the object's internal variables
+    self.name = name
+    self.gpio_number = gpio
+    self.room = room
+    self.onoff_slot = onoff_slot
+    self.counter_slot = counter_slot
+    self.onoff_label = onoff_label
+    self.counter_label = counter_label
+    self.reading = -1
+    self.reading_previous = -1
+    self.debounce_counter = 0
+    self.debounce_max = 2
+    self.stable_state = -1
 
-# Pi Land slots for each button 
-button1_onoff_slot = 13                   # Slot to put button1 on/off state
-button1_counter_slot = 14                 # Slot number to put button1 press counter
-button1_onoff_label = "Button1+Status"    # Label for button on/off slot; '+' will be space
-button1_counter_label = "Button1+Counter" # Label for button on/off slot; '+' will be space
+  def init_gpio(self):
+    # Function to initialize a button's GPIO pin
+    GPIO.setmode(GPIO.BCM)                   # Set BCM pin numbering scheme
+    GPIO.setwarnings(False)
+    GPIO.setup(self.gpio_number, GPIO.IN)    # Set pin as input
 
-# Pi Land REST API endpoint URLs
-baseurl = "http://piland.socialdevices.io"
-baseurl_write = baseurl + "/" + str(piland_room) + "/write/%d?name=%s&value=%s"
-baseurl_incr  = baseurl + "/" + str(piland_room) + "/incr/%d?name=%s"
-baseurl_incr_init = baseurl + "/" + str(piland_room) + "/write/%d?name=%s&value=0"
+  def init_counter(self):
+    # Function to initialize button counter slot
+    url = baseurl_incr_init % (self.room, self.counter_slot, self.counter_label)
+    print url
+    requests.get(url)                        # Call Pi Land API
 
-# Initialize GPIO pins
-GPIO.setmode(GPIO.BCM)                    # Set BCM pin numbering scheme
-GPIO.setwarnings(False)
-GPIO.setup(button1_gpio_number, GPIO.IN)  # Set Button1 pin as input
-print "GPIO pins initialized"
-
-# Initialize button counter to 0
-
-# Button 1
-resturl = baseurl_incr_init % (button1_counter_slot, button1_counter_label)
-print resturl                             # Print the API call URL
-requests.get(resturl)                     # Call Pi Land API
-print "Button1 counter cleared to 0"
-
-# Endless loop reading button and updating Pi Land data
-
-do_incr = False
-
-print "Entering endless loop"
-
-while True:                               # Endless loop until press Control-C
-  
-  try:
-
-    # Read and debounce button 1
-
-    button1_reading = GPIO.input(button1_gpio_number)     # Read current state of button GPIO pin
-
-    # Debouncing logic
-
-    if button1_reading != button1_reading_previous:
-      button1_debounce_counter = 0                        # Reset debounce counter to 0
+  def read_button(self):
+    # Function to read and debounce button, and update values in Pi Land
+    self.reading = GPIO.input(self.gpio_number)      # Read current state of button GPIO pin
+    if self.reading != self.reading_previous:
+      self.debounce_counter = 0                      # Reset debounce counter to 0
     else:
-      if button1_debounce_counter < button1_debounce_max:
-        button1_debounce_counter = button1_debounce_counter + 1
-      if button1_debounce_counter == button1_debounce_max:
+      if self.debounce_counter < self.debounce_max:
+        self.debounce_counter = self.debounce_counter + 1
+      if self.debounce_counter == self.debounce_max:
         # We have stable 0->1 or 1->0 transition
-        button1_stable_state = button1_reading
-        print "button1 =", button1_stable_state
-        button1_debounce_counter = button1_debounce_max + 1   # Set to max+1 so won't trigger again
+        self.stable_state = self.reading
+        print self.name + " = " + str(self.stable_state)
+        self.debounce_counter = self.debounce_max + 1   # Set to max+1 so won't trigger again
         # Set button on/off state in Pi Land slot
-        if button1_stable_state == 0:    # Button is ON (0 = on) 
-          sendurl = baseurl_write % (button1_onoff_slot, button1_onoff_label, "on")
-          do_incr = True                 # When button off -> on we should increment the counter
-        else:                            # Button is OFF (1 = off) 
-          sendurl = baseurl_write % (button1_onoff_slot, button1_onoff_label, "off")
+        if self.stable_state == 0:  # Button is ON (0 = on)
+          # Increment button counter in Pi Land slot
+          sendurl = baseurl_incr % (self.room, self.counter_slot, self.counter_label)
+          print sendurl                  # Print the API call URL
+          requests.get(sendurl)          # Call Pi Land API to increment button counter
+          sendurl = baseurl_write % (self.room, self.onoff_slot, self.onoff_label, "on")
+        else:                            # Button is OFF (1 = off)
+          sendurl = baseurl_write % (self.room, self.onoff_slot, self.onoff_label, "off")
 
         print sendurl                    # Print the API call URL
         requests.get(sendurl)            # Call Pi Land API to set button on/off state in slot
 
-        if do_incr == True:
-          do_incr = False
-          # Increment button counter in Pi Land slot
-          sendurl = baseurl_incr % (button1_counter_slot, button1_counter_label)
-          print sendurl                  # Print the API call URL
-          requests.get(sendurl)          # Call Pi Land API to increment button counter
+    self.reading_previous = self.reading
 
-    button1_reading_previous = button1_reading
+# Pi Land REST API endpoint URLs
 
-    time.sleep(button1_debounce_delay)   # Short delay until read button again
+baseurl = "http://piland.socialdevices.io"
+baseurl_write = baseurl + "/%d/write/%d?name=%s&value=%s"
+baseurl_incr  = baseurl + "/%d/incr/%d?name=%s"
+baseurl_incr_init = baseurl + "/%d/write/%d?name=%s&value=0"
+
+# Button variables
+
+button1 = Button("button1", 17, 404, 13, 14, "Button1+Status", "Button1+Counter")
+
+# Main program
+
+button1.init_gpio()                       # Initialize button 1 GPIO pin
+
+print "Button GPIO pins initialized"
+
+button1.init_counter()                    # Initialize button 1 counter
+
+print "Button counters cleared to 0"
+
+# Endless loop reading button and updating Pi Land data
+
+print "Entering endless loop"
+
+while True:                              # Endless loop until press Control-C
+
+  try:
+
+    button1.read_button()                # Read and process button 1
+
+    time.sleep(0.01)                     # Short delay until read buttons again
 
   except KeyboardInterrupt:              # Handle Control-C
     print ""
